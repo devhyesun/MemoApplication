8f083802e2c4f8e0f7cbde4bdc328fe896ce4ac5
hyesun shin <yn6201@naver.com>
2020-02-18 21:58:14 +0900

    [MainActivity] 패키지 변경
    - view 하위로 이동

--- app/src/main/java/com/hyesun/memoapp/view/MainActivity.kt
+++ app/src/main/java/com/hyesun/memoapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.hyesun.memoapp
+package com.hyesun.memoapp.view
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.hyesun.memoapp.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -37,3 +38,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
