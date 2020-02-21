45b14cba9f357dfc1741c87b8327938a50024cf9
hyesun shin <yn6201@naver.com>
2020-02-21 22:33:19 +0900

    [Util] Utils goToGallery 메소드 추가

--- app/src/main/java/com/hyesun/memoapp/util/Utils.kt
+++ app/src/main/java/com/hyesun/memoapp/util/Utils.kt
@@ -40,6 +40,12 @@ object Utils {
         return photoURI.toString()
     }
 
+    fun goToGallery(activity: Activity, requestCode: Int) {
+        activity.startActivityForResult(Intent(Intent.ACTION_PICK).apply {
+            type = MediaStore.Images.Media.CONTENT_TYPE
+        }, requestCode)
+    }
+
     @SuppressLint("SimpleDateFormat")
     @Throws(IOException::class)
     private fun createImageFile(activity: Activity): File {
