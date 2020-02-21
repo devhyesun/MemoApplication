4e141a69eb7c778e8861a6ce14ceb8a444b76546
hyesun shin <yn6201@naver.com>
2020-02-21 22:21:14 +0900

    [Util] Utils 추가
    - 카메라로 사진 찍어 가져오는 메소드 추가

---
+++ app/src/main/java/com/hyesun/memoapp/util/Utils.kt
@@ -0,0 +1,55 @@
+package com.hyesun.memoapp.util
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+object Utils {
+    fun takePicture(activity: Activity, requestCode: Int): String? {
+        var photoURI: Uri? = null
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(activity.packageManager)?.also {
+                val photoFile = try {
+                    createImageFile(activity)
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                    null
+                }
+                photoFile?.also {
+                    photoURI = FileProvider.getUriForFile(
+                        activity,
+                        "com.hyesun.memoapp",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    activity.startActivityForResult(takePictureIntent, requestCode)
+                }
+
+            }
+        }
+
+        return photoURI.toString()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(IOException::class)
+    private fun createImageFile(activity: Activity): File {
+        val timeStamp = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        Log.i("_hs", "timeStamp : $timeStamp")
+        val pictureDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            timeStamp,
+            ".jpg",
+            pictureDir
+        )
+    }
+}
