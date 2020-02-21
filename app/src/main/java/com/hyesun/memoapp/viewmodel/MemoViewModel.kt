c284960acc8fd2ea7d740e60f5fcc151822056de
hyesun shin <yn6201@naver.com>
2020-02-21 22:12:31 +0900

    [ViewModel] MemoViewModel 각 메소드 Single로 반환하도록 수정

--- app/src/main/java/com/hyesun/memoapp/viewmodel/MemoViewModel.kt
+++ app/src/main/java/com/hyesun/memoapp/viewmodel/MemoViewModel.kt
@@ -11,46 +11,24 @@ import io.reactivex.schedulers.Schedulers
 class MemoViewModel internal constructor(
     private val memoRepository: MemoRepository
 ) : ViewModel() {
-    private val compositeDisposable = CompositeDisposable()
+    val compositeDisposable = CompositeDisposable()
 
     val allMemoList = memoRepository.getAllMemo()
 
-    fun insert(memo: Memo) {
-        compositeDisposable.add(
-            memoRepository.insert(memo)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { i -> Log.i("_hs", "image insert $i success") },
-                    { throwable -> throwable.printStackTrace() }
-                )
-        )
+    fun insert(memo: Memo) =
+        memoRepository.insert(memo)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
-    }
 
-    fun update(memo: Memo) {
-        compositeDisposable.add(
-            memoRepository.update(memo)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { i -> Log.i("_hs", "image update $i success") },
-                    { throwable -> throwable.printStackTrace() }
-                )
-        )
-    }
+    fun update(memo: Memo) =
+        memoRepository.update(memo)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
-    fun delete(memo: Memo) {
-        compositeDisposable.add(
-            memoRepository.delete(memo)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { i -> Log.i("_hs", "image delete $i success") },
-                    { throwable -> throwable.printStackTrace() }
-                )
-        )
-    }
+    fun delete(memo: Memo) = memoRepository.delete(memo)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 
     fun memo(memoId: Long) = memoRepository.getMemo(memoId)
 
