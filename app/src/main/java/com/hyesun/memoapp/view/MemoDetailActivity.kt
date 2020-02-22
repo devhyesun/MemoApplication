dc8bf06816dbbeaada3ec7d517c41313b9ba7067
hyesun shin <yn6201@naver.com>
2020-02-22 21:39:07 +0900

    [MemoDetailActivity] MenuDetailActivity 추가

---
+++ app/src/main/java/com/hyesun/memoapp/view/MemoDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.hyesun.memoapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyesun.memoapp.R
+
+class MemoDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_detail)
+    }
+}
