ea00b4ea431cc3bd3ad189fd437dc6d1b7d70c3f
hyesun shin <yn6201@naver.com>
2020-02-23 23:17:21 +0900

    [MemoDetailActivity] Memo와 Image DB에서 읽어와 세팅

--- app/src/main/java/com/hyesun/memoapp/view/MemoDetailActivity.kt
+++ app/src/main/java/com/hyesun/memoapp/view/MemoDetailActivity.kt
@@ -2,12 +2,61 @@ package com.hyesun.memoapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hyesun.memoapp.R
+import com.hyesun.memoapp.adapter.ImageAdapter
+import com.hyesun.memoapp.util.InjectorUtils
+import com.hyesun.memoapp.viewmodel.ImageViewModel
+import com.hyesun.memoapp.viewmodel.MemoViewModel
+import kotlinx.android.synthetic.main.activity_memo_detail.*
+import kotlinx.android.synthetic.main.row_memo.tvContent
+import kotlinx.android.synthetic.main.row_memo.tvTitle
 
 class MemoDetailActivity : AppCompatActivity() {
+    var memoViewModel: MemoViewModel? = null
+    var imageViewModel: ImageViewModel? = null
+
+    var memoId: Long = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo_detail)
+
+        memoId = intent.getLongExtra("memoId", -1)
+
+        memoViewModel = ViewModelProvider(
+            this,
+            InjectorUtils.provideMemoViewModelFactory(this)
+        ).get(MemoViewModel::class.java)
+
+        imageViewModel = ViewModelProvider(
+            this,
+            InjectorUtils.provideImageViewModelFactory(this)
+        ).get(ImageViewModel::class.java)
+
+        if (memoId > -1) {
+            val adapter = ImageAdapter(this, false)
+            rvImageList.layoutManager = LinearLayoutManager(this)
+            rvImageList.adapter = adapter
+
+            memoViewModel?.memo(memoId)?.observe(this, Observer {
+                if (it != null) {
+                    tvTitle.text = it.title
+                    tvContent.text = it.content
+                }
+            })
+
+            imageViewModel?.imageList(memoId)?.observe(this, Observer {
+                rvImageList.visibility = if (it.isNotEmpty()) {
+                    adapter.setImagePathList(it)
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            })
+        }
     }
 }
