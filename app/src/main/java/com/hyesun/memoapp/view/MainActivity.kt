bb2f578b1c6b12f89e534a7a76c8f26ef4e52e12
hyesun shin <hyesun.shin@kt.com>
2020-02-24 11:46:02 +0900

    [MainActivity] 메뉴 삭제

--- app/src/main/java/com/hyesun/memoapp/view/MainActivity.kt
+++ app/src/main/java/com/hyesun/memoapp/view/MainActivity.kt
@@ -3,8 +3,6 @@ package com.hyesun.memoapp.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -43,20 +41,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
 
